53795ccc260cea0c870e8c6090ab43ca447e0929
Andrey Tumerkin <tumerrkin@gmail.com>
2024-06-11 19:37:09 +0300

    add other device

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(number: str) -> str:
     """Маскирует номер карты"""
-    mask_number = number[0:-12] + 2 * "*" + " " + 4 * "*" + " " + number[-4:]
+    mask_number = number[0:-12] + " " + number[-12:-10] + 2 * "*" + " " + 4 * "*" + " " + number[-4:]
 
     return mask_number
 
