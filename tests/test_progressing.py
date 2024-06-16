d498ed129fb339aebcff293886117b34ef7b40b0
Andrey Tumerkin <tumerrkin@gmail.com>
2024-06-16 21:38:55 +0300

    fixes error

--- tests/test_progressing.py
+++ tests/test_progressing.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.mark.parametrize(
@@ -9,7 +9,17 @@ from src.processing import filter_by_state
         ([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}], []),
         ([{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}], []),
         (
-            [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}],
-            [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}])])
+                [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}],
+                [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}])])
 def test_filter_state(list_info, result):
     assert filter_by_state(list_info) == result
+
+
+@pytest.mark.parametrize("test_list, results",
+                         [
+                             ([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                               {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],
+                              [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])])
+def test_sort_by_data(test_list, results):
+    assert sort_by_date(test_list) == results
