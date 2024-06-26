aa5fc8ac56cd2fce900b2dfea42533c3d5f2f1b3
Andrey Tumerkin <tumerrkin@gmail.com>
2024-06-26 12:58:30 +0300

    fixe code

--- src/decorators.py
+++ src/decorators.py
@@ -13,11 +13,16 @@ def log(filename: Any = None) -> Any:
                 if filename is not None:
                     with open(filename, "w") as file:
                         file.write("my function OK")
+                else:
+                    print("my function OK")
                 return result
             except Exception as e:
                 if filename is not None:
                     with open(filename, "w") as file:
                         file.write(f"my function error:{e}. Input: {args, kwargs}")
+                # else: Не знаю нужно это или нет, на всякий случай(так и непонял что должно выводиться при ошибке)
+                #   print(f"Функция не отрабатывает - введдите корректные данные: Ошибка {e}")
+                #   return f"Функция не отрабатывает - введдите корректные данные: Ошибка {e}"
                 return func(*args, **kwargs)
 
         return wrapper
@@ -31,4 +36,4 @@ def my_function(x, y):
     return x / y
 
 
-print(my_function(10, 1))
+print(my_function(10, 0))
