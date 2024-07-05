87065dce086275260c28a912795ca35aaf214f1b
Andrey Tumerkin <tumerrkin@gmail.com>
2024-07-06 00:05:25 +0300

    add tests

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,13 @@ import json
 
 def read_file(filename: str = None) -> list:
     """Функция считывающая информацию JSON формата с заданного файла"""
-    with open(filename, encoding="utf-8") as file:
-        reading = json.load(file)
-        if type(reading) is not list or len(reading) == 0 or filename is None:
-            return []
-    return reading
+    try:
+        with open(filename, encoding="utf-8") as file:
+            reading = json.load(file)
+            return reading
+    except:
+        return []
 
-# transaction = (read_file("../data/operations.json"))
+
+transaction = (read_file("../data/operations.json"))
+print(transaction)
