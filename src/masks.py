0a396b1f34ec835ce7742c0ad0ad4e3342b6a447
Andrey Tumerkin <tumerrkin@gmail.com>
2024-07-09 19:17:01 +0300

    add new homework

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,31 @@
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('../masks.log')
+file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(number: str) -> str:
     """Маскирует номер карты"""
+    logger.info("Начали маскировку карты")
     if len(number) < 16 or isinstance(number, int):
         return "Ошибка, проверьте правильность ввода."
     else:
         mask_number = number[0:-12] + " " + number[-12:-10] + 2 * "*" + " " + 4 * "*" + " " + number[-4:]
-
+    logger.info("Окончили маскировку карты")
     return mask_number
 
 
 def get_mask_account(account: str) -> str:
     """Маскирует номер счета"""
+    """Это логер для функции get_mask_account"""
+    logger.info("Начали маскировку номера счета")
     inter = str(account)
     if len(account) < 20:
         return "Ошибка, проверьте правильность ввода."
     else:
-        mask_account = "Счет " + 2 * "*" + inter[-4:]
-
-    return mask_account
+        account = "Счет " + 2 * "*" + inter[-4:]
+        logger.info("Окончили маскировку номера счета")
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,57 @@
 import json
+import logging
+import csv
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('../utills.log', "w")
+file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
-def read_file(filename: str = None) -> list:
-    """Функция считывающая информацию JSON формата с заданного файла"""
+def read_file_json(filename: str = None) -> list:
+    """Функция считывающая информацию c JSON файла"""
     try:
+        """Это логер для функции read_file"""
+        logger.info("Начал выгрузку с файла JSON формата")
         with open(filename, encoding="utf-8") as file:
             reading = json.load(file)
-            return reading
-    except:
+            logger.info("Оконили выгрузку с файла JSON формата")
+        return reading
+    except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
         return []
 
 
-transaction = (read_file("../data/operations.json"))
-print(transaction)
+def read_file_csv(filename: str = None) -> list:
+    """Функция считывающая информацию c CSV файла"""
+    try:
+        """Это логер для функции read_file_csv"""
+        logger.info("Начал выгрузку с файла csv формата")
+        with open(filename, encoding="utf-8") as file:
+            reading_csv = csv.DictReader(file, delimiter=";")
+            reading = [read for read in reading_csv]
+        logger.info("Окончили выгрузку с файла csv формата")
+        return reading
+    except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
+        return []
+
+
+def read_file_excel(filename: str = None) -> list:
+    """Функция считывающая информацию c EXCEL файла"""
+    try:
+        """Это логер для функции read_file_excel"""
+        logger.info("Начал выгрузку с файла excel формата")
+        with open(filename, encoding="utf-8") as file:
+            reading_excel = pd.read_excel(file)
+            reading = reading_excel.to_dict()
+        logger.info("Окончили выгрузку с файла excel формата")
+        return reading
+    except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
+        return []
+#
+# print(read_file_csv("../data/trans.xlsx"))
